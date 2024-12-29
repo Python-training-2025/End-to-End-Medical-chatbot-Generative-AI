9726932b979c586d6724a988db8dc2a0ef648af4
Elfn <elimanefofana17@gmail.com>
2024-12-29 08:32:56 +0100

    Set up project as package. Closes #3

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medicalBot Gen AI",
+    version="0.0.0",
+    author="Elimane Fofana",
+    description="Chatbot médical basé sur l'IA générative",
+    packages=find_packages(),
+    install_requires=[
+        # "langchain",
+        # "pinecone-client",
+        # "openai",
+        # "flask"
+    ]
+    # python_requires=">=3.8",
+)
